39c979e14e196068569362e2477e78f123c025b9
MiichaelJose <michaeljosecraft1@gmail.com>
2024-08-21 23:58:14 -0300

    .

--- src/app/(main)/workspace/page.tsx
+++ src/app/(main)/workspace/page.tsx
@@ -9,25 +9,12 @@ async function getData() {
   return data
 }
 
-function getModal() {
-  console.log('oi');
-  
-  return(
-    <div className="w-full h-full z-10 fixed top-0 left-0 bg-gradient-to-r from-gray-900/70 via-gray-800/15 to-gray-900/70 flex justify-center items-center">
-      <div className="w-5/6 md:w-4/12 h-4/6 md:h-2/4 bg-white rounded-md shadow-2xl shadow-gray-900 flex ">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 justify-end">
-          <path strokeLinecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-        </svg>
 
-      </div>
-    </div>
-  )
-}
 
 export default function Page() {
   const [data, setData] = useState([]) // Estado inicial como array vazio
   const [loading, setLoading] = useState(true)
-  const [modal, setModal] = useState(true)
+  const [modal, setModal] = useState(false)
 
   useEffect(() => {
     async function fetchData() {
@@ -47,10 +34,30 @@ export default function Page() {
   if (loading) {
     return <div>Carregando...</div>
   }
+
+  function statusModal() {
+    if(!modal) {
+      setModal(true)
+    }else {
+      setModal(false)
+    }
+  }
+
+  function getModal() {
+    return(
+      <div className="w-full h-full z-10 absolute top-0 left-0 bg-gradient-to-r from-gray-900/70 via-gray-800/15 to-gray-900/70 flex justify-center items-center">
+        <div className="w-5/6 md:w-4/12 h-4/6 md:h-2/4 bg-gray-500 rounded-md shadow-2xl shadow-gray-900 flex p-3">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-9 ml-auto cursor-pointer text-white" onClick={() => statusModal()}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+          </svg>
+        </div>
+      </div>
+    )
+  }
     
   return (
     <>
-      {modal ? <h1>nao</h1>: getModal()}
+      {modal ? getModal() : ''}
       <h1 className=" text-2xl text-white/80">Workspace</h1>
       <div className="w-full h-5/6 my-5 rounded-sm">
         <div className="flex align-middle items-end">
@@ -59,7 +66,7 @@ export default function Page() {
           </div>
         </div>
         <div className="grid gap-4 w-full h-full bg-gray-600/10 p-2 grid-cols-3 auto-rows-fr">
-          <div className="flex w-full justify-center items-center bg-gray-700 rounded-lg" onClick={() => setModal(!modal)}>
+          <div className="flex w-full justify-center items-center bg-gray-700 rounded-lg" onClick={() => statusModal()}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-20 text-gray-800/60">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
             </svg>
