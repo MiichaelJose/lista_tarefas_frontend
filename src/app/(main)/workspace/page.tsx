33a4e973f5dc837dfff67bd2f349e4f1b5ac7f2c
MiichaelJose <michaeljosecraft1@gmail.com>
2024-08-23 23:48:28 -0300

    input image

--- src/app/(main)/workspace/page.tsx
+++ src/app/(main)/workspace/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { Suspense, useEffect, useState } from "react"
 import Loading from "./loading"
+import Notification from "@/app/components/notification"
 
 type Workspace = {
   name: string
@@ -13,13 +14,11 @@ async function getData() {
 }
 
 async function postWorkspace(base: any) {
-  console.log(base);
   const res = await fetch('http://localhost:3002/workspace', {
     method: 'POST',
     headers: {'content-type':'application/json'},
     body: JSON.stringify(base)
   })
-  console.log(res);
   const data = await res.json()
   return data
 }
@@ -31,6 +30,7 @@ export default function Page() {
   const [nameWorkspace, setNameWorkspace] = useState<Workspace>({name: ''})
   const [isSpinner, setIsSpinner] = useState(false)
   const [isNotification, setIsNotification] = useState(false)
+  const [isTypeNotification, setTypeNotification] = useState("")
 
   useEffect(() => {
     async function fetchData() {
@@ -50,18 +50,26 @@ export default function Page() {
     setNameWorkspace({ name: event.target.value })
   }
   
-  const handleSubmit = async () => {
+  async function handleSubmit() {
     setIsSpinner(true)
+    setIsNotification(false)
 
     try {
       const result = await postWorkspace(nameWorkspace)
-      if(!result.ok) {
+
+      if(!result) {
         throw new Error('Erro ao cadastrar workspace.');
       }
+      setIsNotification(true)
+      setTypeNotification('check')
+      //definindo um tempo para não ficar renderizando a cada alteração no estado react (useState)
+      setTimeout(() => {
+        setIsNotification(false);
+      }, 500);
     } catch (error) {
-      console.log(error);
       setIsSpinner(false)
       statusModal()
+      setTypeNotification('error')
       setIsNotification(true)
     } finally {
       setIsSpinner(false)
@@ -83,12 +91,30 @@ export default function Page() {
   function getModal() {
     return(
       <div className="w-full h-full z-10 absolute top-0 left-0 bg-gradient-to-r from-gray-900/50 via-gray-900/80 to-gray-900/50 flex justify-center items-center">
+        {isNotification && 
+          <Notification 
+            type={isTypeNotification}
+          />
+        }
         <div className="relative w-5/6 md:w-3/12  rounded-md  flex flex-col p-3 items-start ">
           <label htmlFor="name" className="text-white/80">Nome</label>
-          <input name="name" type="text" className="w-full h-10 rounded-md outline-none p-3 bg-gray-600 text-white mt-3" onChange={handleChange}/>
+          <div className="flex justify-between items-center content-center flex-row">
+            <input name="name" type="text" className="w-4/4 h-10 rounded-md outline-none p-3 bg-gray-600 text-white " onChange={handleChange}/>
+            <div className="flex items-center justify-center w-10 h-10">
+              <label className="flex flex-col items-center justify-center w-full h-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                      <svg className="w-8 h-8  text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                      </svg>
+                  </div>
+                  <input id="dropzone-file" type="file" className="hidden" />
+              </label>
+            </div> 
+          </div>
+          
           <div className="flex">
             <div className="flex justify-around items-center mt-3 w-28 h-10 bg-gray-700 rounded-md text-white">
-              <button  onClick={handleSubmit}>criar</button>
+              <button onClick={() => handleSubmit()}>criar</button>
             </div>
             {
               isSpinner &&
@@ -100,7 +126,6 @@ export default function Page() {
                 </div>
               </>
             }
-            
             <button className="flex justify-around items-center mt-3 w-28 h-10 bg-gray-700 rounded-md text-white ml-3" onClick={() => statusModal()}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
@@ -117,23 +142,9 @@ export default function Page() {
     <>
       {modal && getModal()}
       {isNotification && 
-        <>
-          <div id="toast-warning" className="fixed top-10 right-10 flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-700" role="alert">
-            <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200">
-                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z"/>
-                </svg>
-                <span className="sr-only">Warning icon</span>
-            </div>
-            <div className="ms-3 text-sm font-normal">Erro interno</div>
-            <button type="button" className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-warning" aria-label="Close">
-                <span className="sr-only">Close</span>
-                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                </svg>
-            </button>
-          </div>
-        </>
+        <Notification 
+          type={isTypeNotification}
+        />
       }
       <h1 className=" text-2xl text-white/80">Workspace</h1>
       <div className="w-full h-5/6 my-5 rounded-sm">
